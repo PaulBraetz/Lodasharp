13fa064afa7590b0a0847897df32dba465ac2719
Paul Brätz <paul.braetz@rhomicro.com>
2024-09-03 22:08:26 +0200

    add sample program

--- Lodashsharp/Program.cs
+++ Lodashsharp/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine();
+﻿using Lodasharp;
+
+JsNode applicationState = [];
+
+JsNode app = [("run", (JsFunc)( (@this, arg) => $"Hello, {arg.Get(0).AsString}!" ))];
+
+applicationState = app.Call("run", applicationState);
+
+Console.WriteLine(applicationState);
