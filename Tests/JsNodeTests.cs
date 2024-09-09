432871fbe66c5149961852b48e92cc26c57780d3
Paul Brätz <paul.braetz@rhomicro.com>
2024-09-09 03:43:45 +0200

    add tests

--- Tests/JsNodeTests.cs
+++ Tests/JsNodeTests.cs
@@ -9,8 +9,40 @@ using System.Xml.Linq;
 
 using Lodasharp;
 
+using static Lodasharp.LsArray;
+
 public class LsNodeTests
 {
+    [Fact]
+    public void LsNode_at_yields_array()
+    {
+        LsNode node = [];
+
+        var actual = node.At();
+
+        Assert.True(actual.IsLsArray);
+        Assert.Empty(actual.AsLsArray);
+    }
+    [Fact]
+    public void LsNode_at_yields_properties()
+    {
+        LsNode node = [("a", 1), ("b", 2), ("c", 3)];
+
+        var actual = node.At("a", "c");
+
+        Assert.True(actual.IsLsArray);
+        Assert.Equal(actual.AsLsArray, Arr(1, 3));
+    }
+    [Fact]
+    public void LsNode_at_yields_nested_properties()
+    {
+        LsNode node = [("a", 1), ("b", Arr([("d", 2)])), ("c", 3)];
+
+        var actual = node.At("b.0.d","a");
+
+        Assert.True(actual.IsLsArray);
+        Assert.Equal(actual.AsLsArray, Arr(2, 1));
+    }
     [Fact]
     public void LsNode_with_invalid_Lson_returns_new_string_value()
     {
